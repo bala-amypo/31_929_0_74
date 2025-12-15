fc2673d0090d72ee2adcc779e08308a87974a3c3
bala-amypo <bala@amypo.in>
2025-12-15 09:43:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,5 +4,45 @@ public class User {
     private long id;
     private String name;
     private String email;
-    private String pa
+    private String password;
+    private String role;
+    public User(){}
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
 }
---
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -0,0 +1,10 @@
+package com.example.demo.entity;
+
+public class Vehicle {
+    private long id;
+    private User user;
+    private String vehicleNumber;
+    private Double capacityKg;
+    private Double fuelEfficiency;
+    
+}
